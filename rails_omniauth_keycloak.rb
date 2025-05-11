c503706c70c2b54f84a71657263732d1795ea0fc
knottedbrain <yilkalargawworkneh@gmail.com>
2025-05-11 21:25:30 +0000

    update the email validation regex in devise.rb

--- rails_omniauth_keycloak.rb
+++ rails_omniauth_keycloak.rb
@@ -99,7 +99,7 @@ after_bundle do
       config.reconfirmable = true
       config.expire_all_remember_me_on_sign_out = true
       config.password_length = 6..128
-      config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+      config.email_regexp = /\\A[^@\\s]+@[^@\\s]+\\z/
       config.reset_password_within = 6.hours
       config.sign_in_after_reset_password = true
       config.sign_out_via = :delete
